8a0afd951c8a50685e14829aa08bb46caa021e53
Andrey Zakharov <killwort@gmail.com>
2020-08-25 21:44:26 +0300

    Inputtable battle start parameters; colorized output; auto fast battle switch operation

--- Program.cs
+++ Program.cs
@@ -1,66 +1,102 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Threading;
-using Capture;
-using Capture.Interface;
 using Tesseract;
-using ImageFormat = System.Drawing.Imaging.ImageFormat;
 
 namespace IBDTools {
-    internal class Program {
-        private static CaptureProcess _captureProcess;
+    partial class Program {
         private static Process _process;
-        private static TesseractEngine _englishOcr, _numbersOcr;
+        private static long _myScore, _myPower, _maxScore = long.MaxValue, _minTickets = 0;
+        private static ManualResetEvent _stopEvent;
 
         public static void Main(string[] args) {
             InitCaptureInterface();
-            var tesseractData = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "tessdata");
-            _englishOcr = new TesseractEngine(tesseractData, "eng");
-            _englishOcr.SetVariable("tessedit_char_whitelist", "qwertyuiopasdfghjklzxcvbnm QWERTYUIOPASDFGHJKLZXCVBNM");
-            _numbersOcr = new TesseractEngine(tesseractData, "eng");
-            _numbersOcr.SetVariable("tessedit_char_whitelist", "0123456789");
+            InitTesseract();
+            Console.Write("\nWelcome to the IBD arena bot.\nPlease enter target score [default = unlimited]: ");
+            while (true) {
+                var l = Console.ReadLine();
+                if (!string.IsNullOrEmpty(l)) {
+                    if (long.TryParse(l, out _maxScore) && _maxScore > 0) break;
+                    Console.Write("Invalid value, try again: ");
+                } else break;
+            }
+
+            Console.Write("Now enter minimum number of arena tickets to reserve [default 0]: ");
+            while (true) {
+                var l = Console.ReadLine();
+                if (!string.IsNullOrEmpty(l)) {
+                    if (long.TryParse(l, out _minTickets) && _minTickets > 0) break;
+                    Console.Write("Invalid value, try again: ");
+                } else break;
+            }
+
+            Console.WriteLine($"All set, now enter main arena screen (ranking) and press enter!\nThe battle will continue until reaching {_minTickets} tickets or {_maxScore} score.");
+            Console.ReadLine();
 
             _stopEvent = new ManualResetEvent(false);
-            var thr = new Thread(
-                () => {
-                    while (ProcessArenaMainScreen() && ProcessArenaMatcherScreen() && ProcessArenaBattle()) {
-                        if (_stopEvent.WaitOne(0)) return;
-                    }
-                }
-            );
+            var thr = new Thread(ArenaBot);
             thr.Start();
             thr.Join();
+            Console.SetCursorPosition(0, 5);
+        }
+
+        private static void ArenaBot() {
+            Console.Clear();
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("IBD ARENA BOT");
+            while (ProcessArenaMainScreen() && ProcessArenaMatcherScreen() && ProcessArenaBattle()) {
+                Console.ForegroundColor = ConsoleColor.Gray;
+                if (_stopEvent.WaitOne(0)) return;
+            }
         }
 
         private static bool ProcessArenaBattle() {
-            Console.WriteLine("Entering battle");
+            Console.SetCursorPosition(0, 1);
+            Console.WriteLine("Entering battle                                                     ");
             WinApi.SendClickAlt(_process.MainWindowHandle, 753, 439);
             Thread.Sleep(2000);
-            Console.WriteLine("Return to main arena screen");
+            Console.SetCursorPosition(0, 1);
+            Console.WriteLine("Return to main arena screen                                         ");
             WinApi.SendClickAlt(_process.MainWindowHandle, 788, 121);
             Thread.Sleep(500);
             return true;
         }
 
         private static bool ProcessArenaMatcherScreen() {
-            Console.WriteLine("Choosing opponent");
+            Console.SetCursorPosition(0, 1);
+            Console.WriteLine("Choosing opponent                                                   ");
             while (true) {
                 if (_stopEvent.WaitOne(0)) return false;
-                _myPower = NumberFromScreen(new Rectangle(250, 175, 460, 19));
+                var fullBitmap = FullScreenshot();
+                _myPower = NumberFromBitmap(fullBitmap, new Rectangle(250, 175, 460, 19));
                 if (_myPower == 0) {
+                    Console.SetCursorPosition(0, 2);
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine($"You're not at the arena matcher screen (or your power is zero), stopping");
                     return false;
                 }
 
-                var v1Power = NumberFromScreen(new Rectangle(337, 295, 158, 18));
-                var v2Power = NumberFromScreen(new Rectangle(337, 396, 158, 18));
-                var v3Power = NumberFromScreen(new Rectangle(337, 499, 158, 18));
-                Console.WriteLine($"Found opponents with powers {v1Power} {v2Power} {v3Power}, my power {_myPower}");
+                var ticketsLeft = NumberFromBitmap(fullBitmap, new Rectangle(495, 68, 100, 19));
+                if (ticketsLeft <= _minTickets) {
+                    Console.SetCursorPosition(0, 2);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Minimum tickets reached, stopping                   ");
+                    return false;
+                }
+
+                var pixColor = fullBitmap.GetPixel(679, 216);
+                var fastBattleIsChecked = pixColor.G > 200;
+                if (!fastBattleIsChecked)
+                    WinApi.SendClickAlt(_process.MainWindowHandle, 679, 216);
+
+                var v1Power = NumberFromBitmap(fullBitmap, new Rectangle(337, 295, 158, 18));
+                var v2Power = NumberFromBitmap(fullBitmap, new Rectangle(337, 396, 158, 18));
+                var v3Power = NumberFromBitmap(fullBitmap, new Rectangle(337, 499, 158, 18));
+                Console.SetCursorPosition(0, 4);
+                Console.WriteLine($"Found opponents with powers {Pretty(v1Power)} {Pretty(v2Power)} {Pretty(v3Power)}, my power {Pretty(_myPower)}                     ");
                 if ((double) v1Power / _myPower < .9 && v1Power > 0) {
                     WinApi.SendClickAlt(_process.MainWindowHandle, 702, 285);
                     return true;
@@ -76,57 +112,26 @@ namespace IBDTools {
                     return true;
                 }
 
-                Console.WriteLine("Opponents are too strong, let's reroll...");
+                Console.SetCursorPosition(0, 1);
+                Console.WriteLine("Opponents are too strong, let's reroll...                            ");
                 WinApi.SendClickAlt(_process.MainWindowHandle, 723, 181);
             }
         }
 
-        private static long _myScore, _myPower;
-        private static ManualResetEvent _stopEvent;
-
         private static bool ProcessArenaMainScreen() {
             var arena = TextFromScreen(new Rectangle(119, 78, 98, 36));
             if (!string.Equals(arena, "arena", StringComparison.InvariantCultureIgnoreCase)) {
-                Console.WriteLine("You're not at the main arena screen, stopping");
+                Console.SetCursorPosition(0, 2);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You're not at the main arena screen, stopping                                ");
                 return false;
             }
 
-            _myScore = NumberFromScreen(new Rectangle(720, 333, 120, 33));
-            Console.WriteLine($"My current score is {_myScore}");
+            var score = NumberFromScreen(new Rectangle(720, 333, 120, 33));
+            Console.SetCursorPosition(0, 3);
+            Console.WriteLine($"My current score is {Pretty(score)}                         ");
             WinApi.SendClickAlt(_process.MainWindowHandle, 737, 394);
             return true;
-        }
-
-        private static string TextFromScreen(Rectangle rt) {
-            var bm = GrabWindowPart(rt);
-            //bm.Save("d:\\last.png", ImageFormat.Png);
-            using (var page = _englishOcr.Process(bm, new Rect(0, 0, rt.Width, rt.Height)))
-                return page.GetText().Trim();
-        }
-
-        private static long NumberFromScreen(Rectangle rt) {
-            var bm = GrabWindowPart(rt);
-            //bm.Save("d:\\last.png", ImageFormat.Png);
-            using (var page = _numbersOcr.Process(bm, new Rect(0, 0, rt.Width, rt.Height)))
-                return long.TryParse(page.GetText().Trim(), out var rv) ? rv : 0;
-        }
-
-        private static Bitmap GrabWindowPart(Rectangle rt) {
-            var scrn = _captureProcess.CaptureInterface.GetScreenshot(rt, TimeSpan.FromSeconds(2), null, Capture.Interface.ImageFormat.Bitmap);
-            var h = scrn.Height;
-            var w = scrn.Width;
-            return new Bitmap(new MemoryStream(scrn.Data));
-        }
-
-        private static void InitCaptureInterface() {
-            _process = Process.GetProcessesByName("DMW").First();
-            WinApi.SetWindowPos(_process.MainWindowHandle, IntPtr.Zero, 0, 0, 1000, 700, 2);
-            CaptureConfig cc = new CaptureConfig() {
-                Direct3DVersion = Direct3DVersion.AutoDetect,
-                ShowOverlay = false
-            };
-            var captureInterface = new CaptureInterface();
-            _captureProcess = new CaptureProcess(_process, cc, captureInterface);
         }
     }
 }
